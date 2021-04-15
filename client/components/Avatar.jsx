9e081aac95f1db00f0355c82901541eb9b563f3e
ysabel-guiang <ysabelregino@yahoo.com>
2021-04-16 11:15:33 +1200

    conflicts fixed

--- client/components/Avatar.jsx
+++ client/components/Avatar.jsx
@@ -1,25 +1,11 @@
 import React from 'react'
 
 function Avatar () {
-<<<<<<< HEAD
     return (
         <div>
-            <img class="ui medium circular image" src={'https://avatars.dicebear.com/api/' + gender + '/' + userInput + '.svg'}/>
+            <img class="ui medium circular image" src={'https://avatars.dicebear.com/api/' + gender + '/' + name + '.svg'}/>
         </div>
     )
-||||||| b82ac6d
-    return (
-        <div>
-            <img src={'https://avatars.dicebear.com/api/' + gender + '/' + userInput + '.svg'} />
-        </div>
-    )
-=======
-  return (
-    <div>
-      <img src={'https://avatars.dicebear.com/api/' + gender + '/' + name + '.svg'} />
-    </div>
-  )
->>>>>>> main
 }
 
 export default Avatar
--- client/components/LoginStyle.jsx
+++ client/components/LoginStyle.jsx
@@ -4,21 +4,10 @@ import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
 const LoginStyle = () => {
   return (
     <>
-<<<<<<< HEAD
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
       <Grid.Column style={{ maxWidth: 450 }}>
       <Header as='h2' color='teal' textAlign='center'>
-||||||| b82ac6d
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='teal' textAlign='center'>
-=======
-      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as='h2' color='teal' textAlign='center'>
->>>>>>> main
          Create your Avatar
-<<<<<<< HEAD
       </Header>
       <Form size='large'>
         <Segment stacked>
@@ -29,54 +18,13 @@ const LoginStyle = () => {
             <option value="female">Female</option>
           </select>
           <Button color='teal' fluid size='large'>
-||||||| b82ac6d
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='Name' name="name" />
-          <select name="gender" className="ui dropdown" id="select">
-            <option value="">Pick your Person</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select>
-
-          <Button color='teal' fluid size='large'>
-=======
-          </Header>
-          <Form size='large'>
-            <Segment stacked>
-              <Form.Input fluid icon='user' iconPosition='left' placeholder='Name' name="name" />
-              <select name="gender" className="ui dropdown" id="select">
-                <option value="">Pick your Person</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
-
-              <Button color='teal' fluid size='large'>
->>>>>>> main
             Create Avatar
-<<<<<<< HEAD
           </Button>
         </Segment>
       </Form>
       </Grid.Column>
     </Grid>
   </>
-||||||| b82ac6d
-          </Button>
-        </Segment>
-      </Form>
-    </Grid.Column>
-  </Grid>
-  </>
-=======
-              </Button>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    </>
->>>>>>> main
   )
 }
 
